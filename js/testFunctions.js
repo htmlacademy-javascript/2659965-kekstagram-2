1a32754351d83cbf754168f0c2ccaaebb0743ec0
KLapkina <lapkinaaksenia@gmail.com>
2026-04-23 21:28:21 +0300

    Исправляет ошибки, выявленные линтером

--- js/testFunctions.js
+++ js/testFunctions.js
@@ -1,39 +1,41 @@
-console.log("Результаты тестов для функции проверки длины строки:");
+import { checkStringLength, checkPalindrome, checkPalindromeTwoPointers, chooseNumbers } from './functions.js';
+/* eslint-disable no-console */
+console.log('Результаты тестов для функции проверки длины строки:');
 // Строка короче 20 символов
-checkStringLength('проверяемая строка', 20); // true
+console.log(checkStringLength('проверяемая строка', 20)); // true
 // Строка короче 5 символов
-checkStringLength('проверяемая строка', 5); // false
+console.log(checkStringLength('проверяемая строка', 5)); // false
 // Длина строки ровно 18 символов
-checkStringLength('проверяемая строка', 18); // true
+console.log(checkStringLength('проверяемая строка', 18)); // true
 // Строка короче 10 символов
-checkStringLength('проверяемая строка', 10); // false
+console.log(checkStringLength('проверяемая строка', 10)); // false
 
-console.log("Результаты тестов для функции проверки строки на палиндром методом переворота строки:");
+console.log('Результаты тестов для функции проверки строки на палиндром методом переворота строки:');
 // Строка является палиндромом
-checkPalindrome('топот'); // true
+console.log(checkPalindrome('топот')); // true
 // Несмотря на разный регистр, тоже палиндром
-checkPalindrome('ДовОд'); // true
+console.log(checkPalindrome('ДовОд')); // true
 // Это не палиндром
-checkPalindrome('Кекс');  // false
+console.log(checkPalindrome('Кекс')); // false
 // Это палиндром
-checkPalindrome('Лёша на полке клопа нашёл '); // true
+console.log(checkPalindrome('Лёша на полке клопа нашёл ')); // true
 
-console.log("Результаты тестов для функции проверки строки на палиндром методом двух указателей:");
+console.log('Результаты тестов для функции проверки строки на палиндром методом двух указателей:');
 // Строка является палиндромом
 console.log(checkPalindromeTwoPointers('топот')); // true
 // Несмотря на разный регистр, тоже палиндром
 console.log(checkPalindromeTwoPointers('ДовОд')); // true
 // Это не палиндром
-console.log(checkPalindromeTwoPointers('Кекс'));  // false
+console.log(checkPalindromeTwoPointers('Кекс')); // false
 // Это палиндром
 console.log(checkPalindromeTwoPointers('Лёша на полке клопа нашёл ')); // true
 
-console.log("Результаты тестов для функции выбора чисел из строки:");
-console.log(chooseNumbers('2023 год'));            // 2023
-console.log(chooseNumbers('ECMAScript 2022'));     // 2022
+console.log('Результаты тестов для функции выбора чисел из строки:');
+console.log(chooseNumbers('2023 год')); // 2023
+console.log(chooseNumbers('ECMAScript 2022')); // 2022
 console.log(chooseNumbers('1 кефир, 0.5 батона')); // 105
-console.log(chooseNumbers('агент 007'));           // 7
-console.log(chooseNumbers('а я томат'));           // NaN
+console.log(chooseNumbers('агент 007')); // 7
+console.log(chooseNumbers('а я томат')); // NaN
 console.log(chooseNumbers(2023)); //2023
-console.log(chooseNumbers(-1));           // 1
-console.log(chooseNumbers(1.5));           // 15
+console.log(chooseNumbers(-1)); // 1
+console.log(chooseNumbers(1.5)); // 15
