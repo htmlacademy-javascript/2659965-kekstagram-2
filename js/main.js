eaf382b08a3638e2ecf5de2fae2bb4d5bed7e3b5
KLapkina <lapkinaaksenia@gmail.com>
2026-05-09 22:50:05 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -1,62 +1,31 @@
 /* eslint-disable no-console */
-const DESCRIPTION = `Всё отлично!
-В целом кадр неплохой. Композиция хромает.
-Когда вы делаете снимок на телефон, уберите палец с угла кадра. В конце концов, это просто дилетантизм.
-Мой кот случайно прошёлся по кнопке спуска, и у него получилась фотография с лучшим фокусом.
-Я поскользнулся на мокром полу, нажал на затвор и снял кадр резче, чем вы.
-Лица на вашем фото перекошены, как будто их снимали через широкоугольник вплотную. Как можно было так испортить свет и ракурс?!`;
+const MESSAGES_ARRAY = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const DESCRIPTION_ARRAY = ['Всё отлично!','В целом кадр неплохой. Композиция хромает.','Когда вы делаете снимок на телефон, уберите палец с угла кадра. В конце концов, это просто дилетантизм.','Мой кот случайно прошёлся по кнопке спуска, и у него получилась фотография с лучшим фокусом.','Я поскользнулся на мокром полу, нажал на затвор и снял кадр резче, чем вы.','Лица на вашем фото перекошены, как будто их снимали через широкоугольник вплотную. Как можно было так испортить свет и ракурс?!'];
 const NAMES = ['Артём', 'Карина', 'Илья', 'Мария', 'Даниил', 'ОЛьга', 'Михаил', 'Алиса', 'Акакий', 'Серафима', 'Геннадий', 'Зухра', 'Святослав', 'Проскофья'];
-const MESSAGES_STR = `Всё отлично!
-В целом всё неплохо. Но не всё.
-Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.
-Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.
-Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.
-Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`;
-
-
-const parseMessages = function (messageString) {
-  const resulStringArray = [];
-  let string = '';
-  for (let i = 0; i < messageString.length; i++) {
-    if (messageString[i] === '\n') {
-      resulStringArray.push(string);
-      string = '';
-    } else {
-      string += messageString[i];
-    }
-  }
-  return resulStringArray;
-};
 
 const createRandomRangeInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-
 const getRandomArrayElement = (array) => array[createRandomRangeInteger(0, array.length - 1)];
-
-const createIdGenerator = (min, max) => {
-  const arrayID = [];
-  for (let i = min; i <= max; i++) {
-    arrayID.push(i);
+const createIdGenerator = () => {
+  const arrayIds = [];
+  for (const i of arrayIds) {
+    arrayIds.push(i);
   }
-  for (let i = 0; i <= arrayID.length - 1; i++) {
+  for (let i = 0; i <= arrayIds.length - 1; i++) {
     const j = createRandomRangeInteger(0,i);
-    const swap = arrayID[i];
-    arrayID[i] = arrayID[j];
-    arrayID[j] = swap;
+    [arrayIds[i], arrayIds[j]] = [arrayIds[j], arrayIds[i]];
   }
   let currentIndex = 0;
   return () => {
-    if (currentIndex >= arrayID.length) {
+    if (currentIndex >= arrayIds.length) {
       return null;
     }
-    const id = arrayID[currentIndex];
+    const id = arrayIds[currentIndex];
     currentIndex++;
     return id;
   };
 };
 
 
-const MESSAGES_ARRAY = parseMessages(MESSAGES_STR);
-const DESCRIPTION_ARRAY = parseMessages(DESCRIPTION);
 const generateCommentId = createIdGenerator(1, 100000);
 const generatePhotoId = createIdGenerator(1, 25);
 const generatePhotoNumber = createIdGenerator(1, 25);
@@ -97,13 +66,16 @@ const createDescriptionPhoto = function () {
   };
 };
 
+// const createDescriptionsPhotoArray = function () {
+//   const descriptionsPhotoArray = [];
+//   descriptionsPhotoArray.length = 25;
+//   for (let i = 0; i <= descriptionsPhotoArray.length - 1; i++) {
+//     descriptionsPhotoArray[i] = createDescriptionPhoto();
+//   }
+//   return descriptionsPhotoArray;
+// };
 const createDescriptionsPhotoArray = function () {
-  const descriptionsPhotoArray = [];
-  descriptionsPhotoArray.length = 25;
-  for (let i = 0; i <= descriptionsPhotoArray.length - 1; i++) {
-    descriptionsPhotoArray[i] = createDescriptionPhoto();
-  }
-  return descriptionsPhotoArray;
+  return Array.from ({length: 25}, createDescriptionPhoto);
 };
 
 
