19db404da375cd36f1d8789c108959dcbed0a998
KLapkina <lapkinaaksenia@gmail.com>
2026-05-11 23:19:15 +0300

    функции и тесты для проверки времени встречи в границах рабочего дня

--- js/functions.js
+++ js/functions.js
@@ -1,41 +1,51 @@
-
-export const checkStringLength = (str, maxLength) => str.length <= maxLength;
-
-export const checkPalindrome = (str) => {
-  const normalizedString = str.toLowerCase().replaceAll(' ', '');
-  let reverseString = '';
-  for (const char of [...normalizedString].reverse()){
-    reverseString += char;
-  }
-  return normalizedString === reverseString;
+const parseTime = (stringTime) => stringTime.split(':').map(Number);
+const timeToMinutes = function (stringTime) {
+  const parsedString = parseTime(stringTime);
+  return parsedString[0] * 60 + parsedString[1];
 };
 
-export const checkPalindromeTwoPointers = (str) => {
-  const normalizedString = str.toLowerCase().replaceAll(' ','');
-  let leftPointer = 0;
-  let rightPointer;
-  rightPointer = normalizedString.length - 1;
-  while (leftPointer < rightPointer) {
-    if (normalizedString[leftPointer] !== normalizedString[rightPointer]) {
-      return false;
-    }
-    leftPointer ++;
-    rightPointer --;
+const isMeetingWithInWorkday = function(beginWorkDay, endWorkDay, beginMeeting, countMinutes) {
+  const workBegin = timeToMinutes(beginWorkDay);
+  const workEnd = timeToMinutes(endWorkDay);
+  const meetBegin = timeToMinutes(beginMeeting);
+  const meetEnd = meetBegin + countMinutes;
+  if (meetBegin >= workBegin && meetEnd <= workEnd) {
+    return true;
   }
-  return true;
+  return false;
 };
 
-export const chooseNumbers = (str) => {
-  let numberString = '';
-  if (typeof str === 'number') {
-    str = str.toString();
-  }
-  for (const char of [...str]){
-    if (!isNaN(parseInt(char,10))) {
-      numberString += char;
-    }
-  }
-  return numberString ? Number(numberString) : NaN;
-};
+export {isMeetingWithInWorkday};
+
+
+// const convertToDate = function (stringTime) {
+//   const date = new Date();
+//   date.setHours(parseTime(stringTime)[0]);
+//   date.setMinutes(parseTime(stringTime)[1]);
+//   return date;
+// };
+// const createTimeObject = function (beginTime, endTime) {
+//   return {
+//     begin: convertToDate(beginTime),
+//     end: convertToDate(endTime)
+//   };
+// };
+// const calculateEndMeeting = function (beginMeeting, countMinutes) {
+//   const beginMeetingMinutes = Number(parseTime(beginMeeting)[0]) * 60 + Number(parseTime(beginMeeting)[1]);
+//   const allMinutes = beginMeetingMinutes + Number(countMinutes);
+//   const hours = Math.floor(allMinutes / 60);
+//   const minutes = allMinutes % 60;
+//   return `${hours}:${minutes}`;
+// };
+// const isMeetingWithInWorkday = function(beginWorkDay, endWorkDay, beginMeeting, countMinutes) {
+//   const endMeeting = calculateEndMeeting(beginMeeting, countMinutes);
+//   const timeWorkDay = createTimeObject(beginWorkDay, endWorkDay);
+//   const timeMeeting = createTimeObject(beginMeeting, endMeeting);
+//   if (timeMeeting.begin >= timeWorkDay.begin && timeMeeting.end <= timeWorkDay.end) {
+//     return true;
+//   }
+//   return false;
+// };
+// export {isMeetingWithInWorkday};
 
 
